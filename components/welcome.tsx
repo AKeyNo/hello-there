d1865c819c474a722a9a849699e735f58b2640ed
Ariel Aquino <arielaquino.cs@gmail.com>
2022-11-24 02:42:51 -0800

    Update packages including Next.js 13

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,5 +1,5 @@
 import { signIn } from 'next-auth/react';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import Link from 'next/link';
 
 const Welcome = (): JSX.Element => {
